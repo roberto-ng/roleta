9a53287abd47f67b1c909580e7c3c0d490ae00b6
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2023-03-11 13:28:51 -0300

    primeiro commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [preact({
+    compat: true
+  })],
+  adapter: netlify()
+});
---
+++ prisma/migrations/20230311141723_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "SorteioGrupoTeste" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+
+    CONSTRAINT "SorteioGrupoTeste_pkey" PRIMARY KEY ("id")
+);
