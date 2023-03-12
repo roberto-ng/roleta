dd6564e91e27902b827ecd08831cc71fa49abd0a
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2023-03-12 17:16:31 -0300

    sorteio de números

---
+++ lib/utils.ts
@@ -0,0 +1,29 @@
+import { randomInt } from "node:crypto";
+import { Range, Set } from "immutable";
+import { trampoline, ThunkOrValue } from 'trampoline-ts';
+
+/** Função recursiva para gerar números aleatórios sem repetir */
+export const gerarNumerosAleatorios = trampoline((
+    min: number, 
+    max: number, 
+    quantidade: number, 
+    encontrados: Set<number> = Set(),
+): ThunkOrValue<Set<number>> => {
+    // gerar número aleatório
+    const resultado = randomInt(min, max);
+    console.log('resultado', resultado);
+
+    if (encontrados.has(resultado)) {
+        // veio um número repetido, buscar outro
+        return gerarNumerosAleatorios.cont(min, max, quantidade, encontrados);
+    }
+
+    const novoEncontrados = encontrados.add(resultado);
+    if (novoEncontrados.size >= quantidade) {
+        // encontramos todos os números, parar o loop
+        return novoEncontrados;
+    } else {
+        // gerar o próximo número
+        return gerarNumerosAleatorios.cont(min, max, quantidade, novoEncontrados);
+    }
+})
--- prisma/zod.ts
+++ prisma/zod.ts
@@ -2,11 +2,15 @@ import { z } from "zod";
 
 export const SorteioNumeroSchema = z.object({
     tipo: z.literal('numero'),
-    nomes: z.string(),
+    numeroMin: z.number().int(),
+    numeroMax: z.number().int(),
+    resultado: z.string().optional(),
+    quantidade: z.number().max(10).optional(),
+    data: z.string().optional(),
 });
 
 export const SorteioNomeSchema = z.object({
     tipo: z.literal('nome'),
-    numeroMin: z.number().int(),
-    numeroMax: z.number().int(),
+    nomes: z.string(),
+    resultado: z.string().optional(),
 });
